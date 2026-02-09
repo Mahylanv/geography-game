a3a69a4a01039628207eb002cdf3d189fc3534d6
Mahylan <mahylan.veclin@wangarden.com>
2026-02-09 16:31:09 +0100

    ecran mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,7 +104,8 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   themeColor: "#0f172a",
-  colorScheme: "light"
+  colorScheme: "light",
+  viewportFit: "cover"
 };
 
 const structuredData = {
